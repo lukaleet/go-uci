465948b21c81b642b1eb974ea8b67962a213f093
lukaleet <2lukaszmaron@gmail.com>
2023-09-30 16:52:36 +0200

    Tidying up, second assignment done

---
+++ week-2/trunc/trunc.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var input float64
+	fmt.Print("Please provide your floating point value: ")
+	fmt.Scan(&input)
+	fmt.Printf("Truncated number is: %d\n", int32(input))
+}
